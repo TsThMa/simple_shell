e041bb98fdd2033c4a2040ad9873c04ae2b2bc79
TsThMa <110995127+TsThMa@users.noreply.github.com>
2022-12-10 11:39:24 +0200

    Collab 2

--- shell.h
+++ shell.h
@@ -9,9 +9,40 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+/* shell_init */
+
 void prompt(void);
 void handle(int signals);
 void _EOF(char *buffer);
 void shell_exit(char **command);
+
+/* create_child */
+
+void create_child(char **command, char *name, char **env, int cicles);
+int change_dir(const char *path);
+
+/* execute */
+
+void execute(char **command, char *name, char **env, int cicles);
+void print_env(char **env);
+char **_getPATH(char **env);
+void msgerror(char *name, int cicles, char **command);
+
+/* tokening */
+
+char **tokening(char *buffer, const char *s);
+
+/* free_mem */
+
+void free_dp(char **command);
+void free_exit(char **command);
+
+/* auxiliar_functions */
+
+int _strcmp(char *s1, char *s2);
+unsigned int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
 
 #endif
